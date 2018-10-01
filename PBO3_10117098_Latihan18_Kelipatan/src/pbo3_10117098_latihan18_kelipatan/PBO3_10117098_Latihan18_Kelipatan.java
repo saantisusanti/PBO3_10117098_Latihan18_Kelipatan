36ebbc13f4cfb17775f080b682b8ee6160cfef19
Santi Susanti <43363256+saantisusanti@users.noreply.github.com>
2018-10-01 14:10:18 +0700

    Add files via upload

---
+++ PBO3_10117098_Latihan18_Kelipatan/src/pbo3_10117098_latihan18_kelipatan/PBO3_10117098_Latihan18_Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117098_latihan18_kelipatan;
+
+/**
+ *
+ * @author 
+ * Nama     : Santi Susanti
+ * KELAS    : PBO3
+ * NIM      : 10117098
+ * Deskripsi Program : Program ini berisi program untuk menampilkan kelipatan 
+ *                     dari 3.5 yang dimulai dari 3.5 sampai 35
+ */
+public class PBO3_10117098_Latihan18_Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(double i = 3.5; i <= 35; i += 3.5){
+            System.out.println(i +" ");
+        }
+    }
+    
+}
